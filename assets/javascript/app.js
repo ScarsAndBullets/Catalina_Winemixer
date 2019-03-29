323041860002f0949b9182e993bd9da144fd5e34
Manny Barboza <mannyb1077@gmail.com>
2019-03-28 21:38:27 -0600

    'Weather'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,59 +12,67 @@ var forecastURL = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/"
 
 console.log(zipcode);
 
+function getWeather() {
+	$.ajax({
+		url: queryURL,
+		method: "GET",
 
-$.ajax({
-	url: queryURL,
-	method: "GET",
+		headers: {
+			"Access-Control-Allow-Origin": "*"
+		}
+	})
+	.then(function(response) {
+		var ajaxResponse = response;
+		console.log("This is response from queryURL " + ajaxResponse);
+		var key = ajaxResponse[0].ParentCity.Key
+		console.log("this is the key")
+		console.log(key)
 
-	headers: {
-		"Access-Control-Allow-Origin": "*"
-	}
-})
-	// We store all of the retrieved data inside of an object called "response"
+		weatherLocationKey = key;
 
-.then(function(response) 
-{
-	var ajaxResponse = response;
-	console.log("This is response from queryURL " + ajaxResponse);
-	// weatherLocationkey = response.ParentCity[;
-	// console.log(weatherLocationKey);
-	var key = ajaxResponse[0].ParentCity.Key
-	console.log("this is the key")
-	console.log(key)
+	});
+} 
 
-	ajaxResponse = key;
+function forecast() {
+	$.ajax({
+		url: forecastURL,
+		method: "GET",
+		headers:
+		{
+			"Access-Control-Allow-Origin": "*"
+		}
+	  })	
+	  .then(function(forecastResponse) {
+			weatherResponse = forecastResponse;
+			console.log(forecastResponse)
+			//Maximum Temp
+			var motherFucker = forecastResponse.DailyForecasts[0].Temperature.Maximum.Value
+			console.log(motherFucker)
+			//Minimum Temp
+			var littleMotherFucker = forecastResponse.DailyForecasts[0].Temperature.Minimum.Value
+			console.log(littleMotherFucker)
+			//Phrase
+			var anotherMotherFucker = forecastResponse.DailyForecasts[0].Day.IconPhrase
+			console.log(anotherMotherFucker)
 
-}); 
-  
-$("#subbutton").on("click", function()
-{
+			$("#fuckthis").text("Maximum Temp: " + motherFucker + " F " + "Minimum Temp: " + littleMotherFucker);
+
+			$("#motherfuckerheader").text(anotherMotherFucker);
+			
+	  });
+}
+   
+$("#subbutton").on("click", function() {
   zipcode = $("#zipcode").val();
   queryURL = "http://dataservice.accuweather.com/locations/v1/search?apikey="+weatherAPIKey+"&q="+zipcode;
- 
-//   weatherLocationkey = response.ParentCity[1];
-//   console.log(weatherLocationKey);
-  //zipcode = newZipcode;
+
   console.log("Zipcode = " + zipcode);
   console.log("This is queryURL " + queryURL);
-  console.log(ajaxResponse);
+  //console.log(ajaxResponse);
 
-
-//   $.ajax({
-// 	url: forecastURL,
-// 	method: "GET",
-// 	headers:
-// 	{
-// 		"Access-Control-Allow-Origin": "*"
-// 	}
-//   })	
-//   .then(function(forecastResponse) 
-//   {
-// 		weatherResponse = forecastResponse;
-// 		console.log("This is forecast response " + forecastResponse);
-// 		//weatherLocationkey = response.ParentCity;		
-//   });
-// 	  console.log("This is forecast URL " + forecastURL);
-	  
+	forecast()
+	console.log("This is forecast URL " + forecastURL);
 	  
 });
+
+getWeather();
