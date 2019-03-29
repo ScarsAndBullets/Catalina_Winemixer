4a9c2007162068035721b55fffc4820ba0bf83fa
Manny Barboza <mannyb1077@gmail.com>
2019-03-28 19:30:04 -0600

    ajax push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,70 @@
 
 
 // This is our API key
-var weatherAPIKey = "q1oW3mN85QbtEooYjAPTYJraVlHLxlHG";
+var weatherAPIKey = "DFl6qNBXCoZzgxeVbBFKpQ62c5sAFz0B"
+//  "q1oW3mN85QbtEooYjAPTYJraVlHLxlHG";
+var weatherLocationKey = "331216";
 var zipcode = "84115";
-//const gmapsKey = "AIzaSyAHg0cxsczfC8WJSv5lhqZ3SAbmGKj_dUQ";
+var ajaxResponse;
+var weatherResponse = "";
 
-var queryURL = `https://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${weatherAPIKey}&q=${zipcode}`;
-// Here we run our AJAX call to the OpenWeatherMap API
+var queryURL = "http://dataservice.accuweather.com/locations/v1/search?apikey="+weatherAPIKey+"&q="+zipcode;
+// var queryURL `https://dataservice.accuweather.com/locations/v1/postalcodes/search?q=16801&apikey=${weatherAPIKey}&q=${zipcode}`;
+var forecastURL = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/"+weatherLocationKey+"?apikey="+weatherAPIKey+"&details=true";
+
+console.log(zipcode);
+
+
 $.ajax({
 	url: queryURL,
 	method: "GET",
 	headers:
-	 {
+	{
 		"Access-Control-Allow-Origin": "*"
 	}
 })
 	// We store all of the retrieved data inside of an object called "response"
-	.then(function(response) {
-		console.log(response);
-  });
-  
-  
-  $("#subbutton").on("click", function()
+.then(function(response) 
 {
- 	alert(" This shit is working ");
-//   newZipcode = $("#zipcode");
-//   zipcode = newZipcode;
-//   console.log("This is zipcode " + newZipcode);
-});
+	var ajaxResponse = response;
+	console.log("This is response from queryURL " + ajaxResponse);
+	// weatherLocationkey = response.ParentCity[;
+	// console.log(weatherLocationKey);
+	var key = ajaxResponse[0].ParentCity.Key
+	console.log("this is the key")
+	console.log(key)
 
-// Map hardcode
-// var queryURL2 = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.4136,%20-111.8926&radius=2500&keyword=coffee&key=${gmapsKey}`;
-// // Here we run our AJAX call to the OpenWeatherMap API
-// $.ajax({
-// 	url: queryURL2,
+	ajaxResponse = key;
+
+}); 
+  
+$("#subbutton").on("click", function()
+{
+  zipcode = $("#zipcode").val();
+  queryURL = "http://dataservice.accuweather.com/locations/v1/search?apikey="+weatherAPIKey+"&q="+zipcode;
+ 
+//   weatherLocationkey = response.ParentCity[1];
+//   console.log(weatherLocationKey);
+  //zipcode = newZipcode;
+  console.log("Zipcode = " + zipcode);
+  console.log("This is queryURL " + queryURL);
+  console.log(ajaxResponse);
+
+//   $.ajax({
+// 	url: forecastURL,
 // 	method: "GET",
-// 	headers: {
+// 	headers:
+// 	{
 // 		"Access-Control-Allow-Origin": "*"
 // 	}
-// })
-// 	// We store all of the retrieved data inside of an object called "response"
-// 	.then(function(response) {
-// 		console.log(response);
-// 	});
-
-//
+//   })	
+//   .then(function(forecastResponse) 
+//   {
+// 		weatherResponse = forecastResponse;
+// 		console.log("This is forecast response " + forecastResponse);
+// 		//weatherLocationkey = response.ParentCity;		
+//   });
+// 	  console.log("This is forecast URL " + forecastURL);
+	  
+	  
+});
