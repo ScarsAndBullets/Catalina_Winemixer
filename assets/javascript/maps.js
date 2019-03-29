4dffc5d3d2e00cc5c3f994091c4e686c7af4874f
Scars and Bullets <sbrough@gmail.com>
2019-03-28 20:29:48 -0600

    successful ajax call

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -11,10 +11,10 @@ var queryURL = `https://api.foursquare.com/v2/venues/search?near=${locationParam
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
 	url: queryURL,
-	method: "GET",
-	headers: {
-		"Access-Control-Allow-Origin": "*"
-	}
+	method: "GET"
+	// headers: {
+	// 	"Access-Control-Allow-Origin": "*"
+	//}
 })
 	// We store all of the retrieved data inside of an object called "response"
 	.then(function(response) {
