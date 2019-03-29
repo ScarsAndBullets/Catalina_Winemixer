3467ef76075f278659a25ecec39c0b2dc3afeaad
ikhall <kimball.hall.code@gmail.com>
2019-03-28 18:19:35 -0600

    second version of UI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,26 +17,16 @@ $.ajax({
 		console.log(response);
   });
   
-  $("#submit").on("click", function()
+  $("#subbutton").on("click", function(e)
 {
-  newZipcode = $("input");
+	e.preventDefault()
+	console.log("Submit did stuff")
+  newZipcode = $("#zipcode").val();
   zipcode = newZipcode;
   console.log("This is zipcode " + zipcode);
+});
+
+$(document).ready(console.log("Ready"))
+function test() {
+	console.log("Test")
 }
-
-// Map hardcode
-var queryURL2 = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.4136,%20-111.8926&radius=2500&keyword=coffee&key=${gmapsKey}`;
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-	url: queryURL2,
-	method: "GET",
-	headers: {
-		"Access-Control-Allow-Origin": "*"
-	}
-})
-	// We store all of the retrieved data inside of an object called "response"
-	.then(function(response) {
-		console.log(response);
-	});
-
-//
